ae827bf44528214011c7ca36649717c28212df13
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2017-12-07 16:48:41 +0000

    looks up postcode and gets lat/long, looks up bus stops within 200m

--- BusBoard.js
+++ BusBoard.js
@@ -43,8 +43,8 @@ request(url, function (error, response, body) {
     console.log('The next five buses:');
     nextBuses.forEach(function(bus) {
         let minutes = parseInt(bus.ETA/60);
-        let minuteString
-        let secondString
+        let minuteString;
+        let secondString;
         if (minutes === 1) {
             minuteString = minutes + ' minute';
         } else {
@@ -60,13 +60,43 @@ request(url, function (error, response, body) {
     })
 });
 
-// code to insert [item] in [array] at [index]: array.splice(index, 0, item)
+console.log('Enter postcode:');
+//let busStop = readline.prompt();
+let postcode = 'N80AH';
+let urlPC = 'https://api.postcodes.io/postcodes/' + postcode
+
+request(urlPC, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('JSON:', body);
+    let data = JSON.parse(body)['result'];
+    // grab latitude and longitude
+    let [longitude, latitude] = [data['longitude'],data['latitude']];
+    let stopTypes = 'NaptanBusCoachStation%2CNaptanBusWayPoint%2CNaptanOnstreetBusCoachStopCluster%2CNaptanOnstreetBusCoachStopPair%2CNaptanPrivateBusCoachTram%2CNaptanPublicBusCoachTram';
+    let urlBS = 'https://api.tfl.gov.uk/StopPoint?stopTypes='+stopTypes+'&radius=200&lat='+latitude+'&lon='+longitude;
+    //call up stop from TFL
+    request(urlBS, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('JSON:', body);
+        let stopdata = JSON.parse(body)['stopPoints'];
+        console.log(stopdata)
+        console.log(stopdata[0]['naptanId'],stopdata[0]['commonName'],stopdata[0]['distance'])
+        console.log(stopdata[1]['naptanId'],stopdata[1]['commonName'],stopdata[1]['distance'])
+    })
+});
 
 
 
-// let entries = JSON.parse(data);
+let urlBS = 'https://api.tfl.gov.uk/StopPoint/' + busStop + '/Arrivals?app_id=' + appID + '&app_key=' + appKey;
+// https://api.tfl.gov.uk/StopPoint?
+// stopTypes=NaptanBusCoachStation%2CNaptanBusWayPoint%2CNaptanOnstreetBusCoachStopCluster%2CNaptanOnstreetBusCoachStopPair%2CNaptanPrivateBusCoachTram%2CNaptanPublicBusCoachTram
+// &radius=200
+// &lat=51.5839674169706
+// &lon=-0.10187318628895
+// &modes=bus
 
-// let transaction = new Transaction(dateParse(entry['Date']), entry['FromAccount'], entry['ToAccount'], entry['Narrative'], entry['Amount']);
+
 
 // {
 //     "$type": "Tfl.Api.Presentation.Entities.Prediction, Tfl.Api.Presentation.Entities",
@@ -99,3 +129,91 @@ request(url, function (error, response, body) {
 //         "received": "0001-01-01T00:00:00Z"
 // }
 // }
+
+// {
+//     "status": 200,
+//     "result": {
+//     "postcode": "N9 0AB",
+//         "quality": 1,
+//         "eastings": 535496,
+//         "northings": 193835,
+//         "country": "England",
+//         "nhs_ha": "London",
+//         "longitude": -0.0438874430749641,
+//         "latitude": 51.6270243729818,
+//         "european_electoral_region": "London",
+//         "primary_care_trust": "Enfield",
+//         "region": "London",
+//         "lsoa": "Enfield 025G",
+//         "msoa": "Enfield 025",
+//         "incode": "0AB",
+//         "outcode": "N9",
+//         "parliamentary_constituency": "Edmonton",
+//         "admin_district": "Enfield",
+//         "parish": "Enfield, unparished area",
+//         "admin_county": null,
+//         "admin_ward": "Lower Edmonton",
+//         "ccg": "NHS Enfield",
+//         "nuts": "Enfield",
+//         "codes": {
+//         "admin_district": "E09000010",
+//             "admin_county": "E99999999",
+//             "admin_ward": "E05000204",
+//             "parish": "E43000200",
+//             "parliamentary_constituency": "E14000687",
+//             "ccg": "E38000057",
+//             "nuts": "UKI54"
+//     }
+// }
+// }
+
+// [
+//     "CarPickupSetDownArea",
+//     "NaptanAirAccessArea",
+//     "NaptanAirEntrance",
+//     "NaptanAirportBuilding",
+//     "NaptanBusCoachStation",
+//     "NaptanBusWayPoint",
+//     "NaptanCoachAccessArea",
+//     "NaptanCoachBay",
+//     "NaptanCoachEntrance",
+//     "NaptanCoachServiceCoverage",
+//     "NaptanCoachVariableBay",
+//     "NaptanFerryAccessArea",
+//     "NaptanFerryBerth",
+//     "NaptanFerryEntrance",
+//     "NaptanFerryPort",
+//     "NaptanFlexibleZone",
+//     "NaptanHailAndRideSection",
+//     "NaptanLiftCableCarAccessArea",
+//     "NaptanLiftCableCarEntrance",
+//     "NaptanLiftCableCarStop",
+//     "NaptanLiftCableCarStopArea",
+//     "NaptanMarkedPoint",
+//     "NaptanMetroAccessArea",
+//     "NaptanMetroEntrance",
+//     "NaptanMetroPlatform",
+//     "NaptanMetroStation",
+//     "NaptanOnstreetBusCoachStopCluster",
+//     "NaptanOnstreetBusCoachStopPair",
+//     "NaptanPrivateBusCoachTram",
+//     "NaptanPublicBusCoachTram",
+//     "NaptanRailAccessArea",
+//     "NaptanRailEntrance",
+//     "NaptanRailPlatform",
+//     "NaptanRailStation",
+//     "NaptanSharedTaxi",
+//     "NaptanTaxiRank",
+//     "NaptanUnmarkedPoint",
+//     "TransportInterchange"
+// ]
+
+// [
+//     "NaptanBusCoachStation",
+//     "NaptanBusWayPoint",
+//     "NaptanOnstreetBusCoachStopCluster",
+//     "NaptanOnstreetBusCoachStopPair",
+//     "NaptanPrivateBusCoachTram",
+//     "NaptanPublicBusCoachTram",
+// ]
+
