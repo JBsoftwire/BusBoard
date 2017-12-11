6c0fda25f217b288ea57d68bb80093de0715606e
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2017-12-11 17:18:49 +0000

    improved time output

--- frontend/departures.js
+++ frontend/departures.js
@@ -9,33 +9,27 @@ class Bus {
 
 function getPostcode() {
     var postcode = document.getElementById("postcode").value;
-    console.log(postcode)
+    console.log('postcode:',postcode)
     var url = 'http://localhost:3000/departureBoards?postcode=' + encodeURIComponent(postcode);
-
     var xhttp = new XMLHttpRequest();
     // xhttp.setRequestHeader('Content-Type', 'application/json');
     xhttp.open('GET', url, true);
-
     xhttp.onload = function() {
-        //handle response here using e.g. xhttp.status, xhttp.response, xhttp.responseText
-        // document.getElementById("results").innerHTML = xhttp.response;
         var buses = JSON.parse(xhttp.response);
         var newHTML = '';
-        console.log(buses);
         for (var i = 0; i < buses.length; i++) {
             console.log(buses[i]);
             newHTML = newHTML + '<h3>' + buses[i][0]['stop'] + '</h3>\n<ul>\n';
             for (var j = 0; j < buses[i].length; j++) {
                 var bus = buses[i][j];
-                console.log(bus);
-                newHTML = newHTML + '<li>' + bus['ETA']/60 + ' minutes: ' + bus['line'] + ' to ' + bus['destination'] + '</li>\n';
+                var minuteString = parseInt(bus['ETA']/60) + ' minutes';
+                var secondString = parseInt(bus['ETA']%60) + ' seconds';
+                newHTML = newHTML+'<li>'+ minuteString +' and '+ secondString +': number '+ bus['line'] +' to '+ bus['destination'] + '</li>\n';
             }
             newHTML = newHTML + '</ul>\n'
         }
-        // console.log(newHTML);
         document.getElementById("results").innerHTML = newHTML
     };
-
     xhttp.send();
 }
 
@@ -60,11 +54,3 @@ function getPostcode() {
 //     [{"line":"341","stop":"Harringay Road","destination":"Angel Road, Superstores","ETA":268},
 //         {"line":"341","stop":"Harringay Road","destination":"Angel Road, Superstores","ETA":931},
 //         {"line":"341","stop":"Harringay Road","destination":"Angel Road, Superstores","ETA":1701}]]
-
-// buses.forEach((location) => {
-//     newHTML = newHTML + '<h3>' + location[0].stop + '</h3>\n<ul>\n';
-//     location.forEach((bus) => {
-//         newHTML = newHTML + '<li>' + bus.ETA/60 + ' minutes: ' + bus.line + ' to ' + bus.destination + '</li>\n';
-//     })
-//     newHTML = newHTML + '</ul>\n'
-// })
